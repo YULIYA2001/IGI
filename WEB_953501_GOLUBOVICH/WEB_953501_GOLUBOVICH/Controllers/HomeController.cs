c454a5b684c1f8ca455d9e4ff328cb542d20c3a5
yuliya <gyuliya2001@gmail.com>
2021-10-08 20:11:33 +0300

    lab 2

--- WEB_953501_GOLUBOVICH/WEB_953501_GOLUBOVICH/Controllers/HomeController.cs
+++ WEB_953501_GOLUBOVICH/WEB_953501_GOLUBOVICH/Controllers/HomeController.cs
@@ -1,37 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WEB_953501_GOLUBOVICH.Models;
 
 namespace WEB_953501_GOLUBOVICH.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private List<ListDemo> _listDemo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
 
         public IActionResult Index()
         {
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+
+            ViewData["Text"] = "Лабораторная работа 2";
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
 }
