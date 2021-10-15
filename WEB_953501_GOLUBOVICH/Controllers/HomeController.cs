e1a93d9336e47bbd9b02dd975e85cd837123f6b0
Yuliya <gyuliya2001@gmail.com>
2021-10-15 20:56:03 +0300

    lab4

---
+++ WEB_953501_GOLUBOVICH/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_953501_GOLUBOVICH.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<ListDemo> _listDemo;
+
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+
+            ViewData["Text"] = "Лабораторная работа 2";
+            return View();
+        }
+
+    }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
+}
